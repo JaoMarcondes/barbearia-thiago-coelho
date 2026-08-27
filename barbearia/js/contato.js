cf3d8598dbb25236944ececd11ebc9870ee98933
João Guilherme <jao.marcondes.2006@gmail.com>
2026-08-27 01:52:26 -0300

    Delete barbearia directory

--- barbearia/js/contato.js
+++
@@ -1,6 +0,0 @@
-
-import { setupChrome, SHOP, whatsappLink } from "./barbearia.js";
-setupChrome();
-const a=document.querySelector("#whatsappContact");
-const wa=whatsappLink("Olá! Gostaria de falar com a barbearia.");
-if(wa){a.href=wa;a.hidden=false;}
